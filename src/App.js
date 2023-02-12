dd775becfdcff2854775a7b1bbe064fdb0a9c694
Zain_Ali_Sidd <114559080+Zain-Ali-Siddiqui@users.noreply.github.com>
2023-02-12 15:36:32 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { useState } from "react";
 import './App.css';
 import Router from "./Config/routes";
+import { AdminHome } from "./Screens";
 function App() {
- 
     return (
       <div>
-        <Router/>
-      </div>
-        
-
+        {/* <Router/> */}
+        <AdminHome/>
+      </div>    
     );
     }
   export default App;
