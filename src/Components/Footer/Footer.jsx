751e4e6085b8e0b385b835c4b6d4668a350cdaec
Rehan Pasha <104275485+Rehanpasha732@users.noreply.github.com>
2023-02-12 04:28:26 -0800

    updated

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,18 +5,21 @@ import Person2Icon from '@mui/icons-material/Person2';
 function Footer() {
     return (
         <div className="footer_main_div">
-            <div className="footer_inner_div">
+            <Link to='/admin/home'>   <div className="footer_inner_div">
                 <HomeIcon />
                 <p> Home </p>
             </div>
-            <div className="footer_inner_div">
+            </Link>
+            <Link to='/admin/add/product'>   <div className="footer_inner_div">
                 <ControlPointIcon />
                 <p> Items </p>
             </div>
-            <div className="footer_inner_div">
+            </Link>
+            <Link to='/admin/prfile'> <div className="footer_inner_div">
                 <Person2Icon />
                 <p> Account </p>
             </div>
+            </Link>
         </div>
     )
 }
