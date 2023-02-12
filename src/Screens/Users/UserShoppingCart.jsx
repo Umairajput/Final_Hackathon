b013088f8bb29b9a3c462a61bf43b348dad0b089
Rehan Pasha <104275485+Rehanpasha732@users.noreply.github.com>
2023-02-12 03:11:45 -0800

    updated

--- src/Screens/Users/UserShoppingCart.jsx
+++ src/Screens/Users/UserShoppingCart.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import profile from '../../Assets/Images/Profile.png'
 
 function UserShoppingCart() {
   return (
-    <div>
-      
-    </div>
+    <>
+
+      <div className="shpping_div">
+        <div className="shopping_proifle">
+          <img src={profile} alt="" />
+        </div>
+
+      </div>
+
+
+    </>
   )
 }
 
