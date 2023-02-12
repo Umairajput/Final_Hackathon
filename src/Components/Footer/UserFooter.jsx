ee5cf747c6e87c83bfa72b03194c4b5d8b79aeb0
Zain_Ali_Sidd <114559080+Zain-Ali-Siddiqui@users.noreply.github.com>
2023-02-12 17:45:34 +0500

    update

--- src/Components/Footer/UserFooter.jsx
+++ src/Components/Footer/UserFooter.jsx
@@ -5,24 +5,29 @@ import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { Link } from 'react-router-dom'
 
 function UserFooter() {
+    const getLocation = window?.location?.pathname
     return (
         <div className="footer_main_div">
-            <Link to='/user/home/page' >   <div className="footer_inner_div">
-                <HomeIcon />
-                <p> Home </p>
-            </div>
+            <Link to='/user/home/page' className="link">
+                <div className="footer_inner_div">
+                    <HomeIcon className={getLocation === "/user/home/page" ? "active" : "footer_icon"} />
+                    <p className={getLocation === "/user/home/page" ? "active" : "footer_icon"}> Home </p>
+                </div>
             </Link>
-            <Link to='/user/shopping/cart'  > <div className="footer_inner_div">
-                <ShoppingCartIcon />
-                <p> Cart </p>
-            </div>
+            <Link to='/user/shopping/cart' className="link" >
+                <div className="footer_inner_div">
+                    <ShoppingCartIcon  className={getLocation === "/user/shopping/cart" ? "active" : "footer_icon"}/>
+                    <p className={getLocation === "/user/shopping/cart" ? "active" : "footer_icon"}>Cart </p>
+                </div>
             </Link>
-            <Link to='/user/profile'> <div className="footer_inner_div">
-                <Person2Icon />
-                <p> Account </p>
-            </div>
+            <Link to='/user/profile' className="link">
+                <div className="footer_inner_div">
+                    <Person2Icon  className={getLocation === "/user/profile" ? "active" : "footer_icon"}/>
+                    <p  className={getLocation === "/user/profile" ? "active" : "footer_icon"}> Account </p>
+                </div>
             </Link>
         </div>
     )
 }
 export default UserFooter
+
