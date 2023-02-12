91250855f000ed029c0b86734d1a87cb6201fa33
Rehan Pasha <104275485+Rehanpasha732@users.noreply.github.com>
2023-02-12 00:41:46 -0800

    create-login-signup

--- src/Screens/Form/Home.jsx
+++ src/Screens/Form/Home.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import Logo from '../../Assets/logo.png'
+import { Link } from 'react-router-dom'
 
 function Home() {
   return (
-    <div>
-      <h1>This is Home Page</h1>
-    </div>
+    <>
+      <div className="getStarted_main_div">
+        <div className="log_saylani_Store"><img src={Logo} alt="" /></div>
+        <div className='saylaniWelfare_heading_div'>
+          <h1>Saylani Welfare </h1>
+          <p>ONLINE DISCOUNT STORE</p>
+        </div>
+        <div className="signup_button_div">
+          <button className='get_Started_button'> <Link to='/register'className='link'  > GET STARTED</Link> </button>
+        </div>
+      </div>
+    </>  
   )
+
 }
 
 export default Home
--- src/Screens/Form/Login.jsx
+++ src/Screens/Form/Login.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import { useState } from "react"
 
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import WifiCallingIcon from '@mui/icons-material/WifiCalling';
+import MarkunreadIcon from '@mui/icons-material/Markunread';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import { Link } from 'react-router-dom'
 const Login = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+
   return (
     <>
-      
+      <div className="Signup_form">
+        <div className="Signup_form_heading">
+          <h1>SAYLANI WELFARE</h1>
+          <p>ONLINE DISCOUNT STORE</p>
+        </div>
+
+        <div className="input-div one">
+          <div className="i">
+            <MarkunreadIcon className='form_icon' />
+          </div>
+          <div className="div">
+            <input type="Email" className="input" placeholder=' Enter Email'
+              value={email} onChange={(e) => {
+                setEmail(e.target.value)
+                console.log(e.target.value)
+              }}
+            />
+          </div>
+        </div>
+
+        <div className="input-div one">
+          <div className="i">
+            <VisibilityOffIcon className='form_icon' />
+          </div>
+          <div className="div">
+            <input type="password" className="input" placeholder='Enter Password'
+              value={password} onChange={(e) => {
+                setPassword(e.target.value)
+                console.log(e.target.value)
+              }}
+
+            />
+          </div>
+        </div>
+        <div className="get_Started_button_div">
+          <button className='get_Started_button'> <Link to='/login' className='link'  > Sign In</Link> </button>
+          <p className="already_account"> <Link to='/register' className='link_login' >Don't Have an account? Register</Link> </p></div>
+
+      </div>
     </>
   )
 }
